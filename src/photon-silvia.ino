652ab6603f89fa7c99d0a91bf59ebe6d823f41c8
Austin Howard <austin.howard@respec.com>
2018-03-10 17:00:45 -0600

    temp reading working

--- src/photon-silvia.ino
+++ src/photon-silvia.ino
@@ -1,18 +1,35 @@
-/*
- * Project photon-silvia
- * Description:
- * Author:
- * Date:
- */
+#include <OneWire.h>
+#include <spark-dallas-temperature.h>
 
-// setup() runs once, when the device is first turned on.
+#ifndef TEMPERATURE_PIN
+#define TEMPERATURE_PIN D6
+#endif
+
+#ifndef RESOLUTION
+#define RESOLUTION 11
+#endif
+
+OneWire oneWire(TEMPERATURE_PIN);
+DallasTemperature sensors(&oneWire);
+
 void setup() {
-  // Put initialization like pinMode and begin functions here.
-  Particle.publish("photon-silvia", "test");
+  Particle.publish("photon-silvia", "Starting");
+
+  sensors.begin();
+  sensors.setResolution(RESOLUTION);
+  if (sensors.isParasitePowerMode()) {
+    Particle.publish("photon-silvia-parasite", "using parasite power mode");
+  } else {
+    Particle.publish("photon-silvia-powered", "using regular power mode");
+  }
 }
 
-// loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  // The core of your code will likely live here.
-
+  Particle.publish("temp-request", "Requesting temp");
+  sensors.requestTemperatures();
+  Particle.publish("temp-requested", "Requested temp");
+  float temperature = sensors.getTempCByIndex(0);
+  String temperatureStr = String(temperature);
+  Particle.publish("current temp", temperatureStr);
+  delay(8000);
 }
